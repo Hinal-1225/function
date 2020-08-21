bbd3c180c8cebc039146a926f6eee18808ed51dc
hinal-025 <68282568+hinal-025@users.noreply.github.com>
2020-08-21 10:22:56 +0530

    Add files via upload

---
+++ C_function.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+void fun_sum(int n);
+int main()
+{
+    system("cls");
+    int i,n;
+    printf("\nEnter number : ");
+    scanf("%d",&n);
+    fun_sum(n);
+    system("pause");
+	return 10;
+}
+void fun_sum(int n)
+{
+    int i=1,sum=0;
+    for(i=1;i<=n;i++)
+    {
+        printf("%d",i);
+        sum=sum+i;
+    }
+    printf("\nThe sum of 1 to %d is : %d ",n,sum);
+}
